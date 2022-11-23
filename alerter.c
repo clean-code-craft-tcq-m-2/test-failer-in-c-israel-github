70412285b31ac0e6abf86b340a5987c63710d367
Arsiel <israhell_oz@hotmail.com>
2022-11-23 17:27:45 -0600

    Improved tests
    Corrected assert statements in tshirts.c.
    Added assert and separated test stub in alerter.c

--- alerter.c
+++ alerter.c
@@ -1,32 +1,26 @@
 #include <stdio.h>
 #include <assert.h>
+#include "testStub.h"
 
 int alertFailureCount = 0;
 
-int networkAlertStub(float celcius) {
-    printf("ALERT: Temperature is %.1f celcius.\n", celcius);
-    // Return 200 for ok
-    // Return 500 for not-ok
-    // stub always succeeds and returns 200
-    return 200;
-}
-
-void alertInCelcius(float farenheit) {
-    float celcius = (farenheit - 32) * 5 / 9;
-    int returnCode = networkAlertStub(celcius);
-    if (returnCode != 200) {
-        // non-ok response is not an error! Issues happen in life!
-        // let us keep a count of failures to report
-        // However, this code doesn't count failures!
-        // Add a test below to catch this bug. Alter the stub above, if needed.
-        alertFailureCount += 0;
-    }
+void alertInCelsius(float fahrenheit) {
+	float celsius = (fahrenheit - 32) * 5 / 9;
+	int returnCode = networkAlertStub(celsius);
+	if (returnCode != 200) {
+		// non-ok response is not an error! Issues happen in life!
+		// let us keep a count of failures to report
+		// However, this code doesn't count failures!
+		// Add a test below to catch this bug. Alter the stub above, if needed.
+		alertFailureCount += 0;
+	}
 }
 
 int main() {
-    alertInCelcius(400.5);
-    alertInCelcius(303.6);
-    printf("%d alerts failed.\n", alertFailureCount);
-    printf("All is well (maybe!)\n");
-    return 0;
+	alertInCelsius(400.5);
+	alertInCelsius(303.6);
+    assert(alertFailureCount == 2);
+	printf("%d alerts failed.\n", alertFailureCount);
+	printf("All is well (maybe!)\n");
+	return 0;
 }
--- tshirts.c
+++ tshirts.c
@@ -2,21 +2,21 @@
 #include <assert.h>
 
 char size(int cms) {
-    char sizeName = '\0';
-    if(cms < 38) {
-        sizeName = 'S';
-    } else if(cms > 38 && cms < 42) {
-        sizeName = 'M';
-    } else if(cms > 42) {
-        sizeName = 'L';
-    }
-    return sizeName;
+	char sizeName = '\0';
+	if(cms < 38) {
+		sizeName = 'S';
+	} else if(cms > 38 && cms < 42) {
+		sizeName = 'M';
+	} else if(cms > 42) {
+		sizeName = 'L';
+	}
+	return sizeName;
 }
 
 int main() {
-    assert(size(37) == 'S');
-    assert(size(40) == 'M');
-    assert(size(43) == 'L');
-    printf("All is well (maybe!)\n");
-    return 0;
-}
+	assert(size(38) == 'S');
+	assert(size(38) == 'M');
+	assert(size(42) == 'L');
+	printf("All is well (maybe!)\n");
+	return 0;
+}	
